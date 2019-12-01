9009823fd21a66fb063106235c96c90ef1eb8cac
awsnickde <56998264+awsnickde@users.noreply.github.com>
2019-12-01 21:25:32 +0100

    create skeleton

---
+++ backend/admin.js
@@ -0,0 +1,10 @@
+
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(name),
+   };
+   return response;
+};
---
+++ backend/gate.js
@@ -0,0 +1,62 @@
+const util = require('util');
+const AWS = require('aws-sdk');
+const rekognition = new AWS.Rekognition();
+
+const tableName = process.env.DYNAMODB_TABLE;
+const collectionID = process.env.REKOGNITION_COLLECTION;
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: process.env.AWS_REGION
+});
+
+//, context, callback
+exports.handler = async (event) => {
+    var body = JSON.parse(event.body);
+    let buff = Buffer.from(body.img, 'base64')//.toString('ascii')
+    var arrByte = Uint8Array.from(buff)
+    
+    //console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
+       var params = {
+            Image: {
+                 Bytes: arrByte
+            },
+            CollectionId: collectionID,
+            MaxFaces: 5,
+        };
+    var name = "Warning! Unauthorized person!"    
+    
+    await rekognition.searchFacesByImage(params).promise().then(data => {
+        console.log(data);
+        if (data.FaceMatches.length > 0) {
+            console.log(data);
+            var foundId = data.FaceMatches[0].Face.FaceId
+            console.log("Found: ", foundId)
+            
+            var params = {
+                TableName : tableName,
+                FilterExpression: "#fid = :faceId",
+                ExpressionAttributeNames:{
+                    "#fid": "faceId"
+                },
+                ExpressionAttributeValues: {
+                    ":faceId": foundId
+                }
+            };
+            return docClient.scan(params).promise().then((data) => {
+                data.Items.forEach(function(item) {
+                    name = "Hello " + item.Username
+                });
+            });
+        } else {
+            console.log("NO MATCH");
+        }
+    }).catch(err => {
+        console.log("ERROR!!!", err)
+    });
+
+    const response = {
+         statusCode: 200,
+         body: JSON.stringify(name),
+    };
+    return response;
+};
